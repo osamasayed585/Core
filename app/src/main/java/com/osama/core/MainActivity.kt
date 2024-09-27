51d4bed97deacf90d083379fe959dfdcaa171f39
Usama <osamasayed585@gmai.com>
2024-09-27 16:32:13 +0300

    change name

--- app/src/main/java/com/osama/core/MainActivity.kt
+++ app/src/main/java/com/osama/core/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CoreTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "DroidOs",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
